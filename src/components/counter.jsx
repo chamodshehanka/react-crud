193281b20106fdcce94f81927cf7b53327c8f5ca
Shehanka <hcsperera@outlook.com>
2019-10-30 00:45:17 +0530

    css classes method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    counter: 10,
+    counter: 0,
     imageUrl:
       "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
   };
@@ -13,17 +13,23 @@ class Counter extends Component {
   };
 
   render() {
-    // React.createElement("div");
     return (
       <React.Fragment>
         <img src={this.state.imageUrl} alt=""></img>
         <br></br>
-        <span style={this.styles} className="badge badge-primary m-2">
+
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2";
+    classes += this.state.counter === 0 ? " badge-warning" : " primary";
+    return classes;
   }
 
   formatCount() {
