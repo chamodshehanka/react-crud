95cc7f79521862d37acdad8e232b6aef0e6d9cfd
Shehanka <hcsperera@outlook.com>
2019-10-30 18:01:19 +0530

    sfc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="!#" value>
-          NavBar{" "}
-          <span className="badge badge-pill badge-secordary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="!#" value>
+        NavBar{" "}
+        <span className="badge badge-pill badge-secordary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
